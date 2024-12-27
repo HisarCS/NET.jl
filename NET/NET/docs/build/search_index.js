41a9530e20ecbe5eb10d1921be1a7c1b49381b02
Emre Dayangaç <120194760+EmreDay1@users.noreply.github.com>
2024-12-27 13:12:29 +0300

    Delete NET/NET directory

--- NET/NET/docs/build/search_index.js
+++
@@ -1,3 +0,0 @@
-var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NET","category":"page"},{"location":"#NET","page":"Home","title":"NET","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NET.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NET]","category":"page"}]
-}
--- NET/NET/test/runtests.jl
+++
@@ -1,6 +0,0 @@
-using NET
-using Test
-
-@testset "NET.jl" begin
-    # Write your tests here.
-end
